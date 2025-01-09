caf0250d224621005ace6e95df141fee5afc5a4a
krasnoem9 <milabozhikovaa@gmail.com>
2025-01-09 17:03:25 +0400

    Delete projectscreation.js

--- projectscreation.js
+++
@@ -1,6 +0,0 @@
-function concatenateData(firstName, projects) {
-    let hours = projects * 3;
-    console.log(`The architect ${firstName} will need ${hours} hours to complete ${projects} project/s.`);
-}
-
-concatenateData("George", 4);
